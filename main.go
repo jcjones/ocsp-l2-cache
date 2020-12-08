ed98ff7da26db6c427e0bc99e0b0db42c04a7ffb
J.C. Jones <jc@insufficient.coffee>
2020-12-07 22:50:25 -0700

    Add some configuration env variables

--- main.go
+++ main.go
@@ -15,6 +15,14 @@ import (
 	blog "github.com/letsencrypt/boulder/log"
 )
 
+func getEnvString(name string, def string) string {
+	setting, ok := os.LookupEnv(name)
+	if !ok {
+		return def
+	}
+	return setting
+}
+
 func main() {
 	const defaultPriority = syslog.LOG_INFO | syslog.LOG_LOCAL0
 	syslogger, err := syslog.Dial("", "", defaultPriority, "test")
@@ -37,10 +45,10 @@ func main() {
 		WithUpstreamResponder("C5B1AB4E4CB1CD6430937EC1849905ABE603E225", "http://ocsp.int-x4.letsencrypt.org").
 		WithUpstreamResponder("142EB317B75856CBAE500940E61FAF9D8B14C2C6", "http://r3.o.lencr.org").
 		WithUpstreamResponder("369D3EE0B140F6272C7CBF8D9D318AF654A64626", "http://r4.o.lencr.org").
-		WithIdentifier("jcj testing").
-		WithListenAddr(":8080").
-		WithHealthListenAddr(":8081").
-		WithRedis("192.168.99.100:6379", time.Second).
+		WithIdentifier(getEnvString("ID", "no ID set")).
+		WithListenAddr(getEnvString("ListenOCSP", ":8080")).
+		WithHealthListenAddr(getEnvString("ListenHealth", ":8081")).
+		WithRedis(getEnvString("RedisHost", "redis:6379"), time.Second).
 		WithCacheLifespan(24 * time.Hour).
 		WithConnectionDeadline(time.Second).
 		// Signals are handled in the CLI package
